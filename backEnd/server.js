6c2bc09725b7727824cf94865b54548fbecc4b48
TechMaestro-k <maheshwarikushagra09@gmail.com>
2025-08-12 01:32:57 +0530

    login,signUp Apis

--- backEnd/server.js
+++ backEnd/server.js
@@ -3,6 +3,7 @@ const express = require("express")
 const cors=require("cors")
 const path=require("path")
 const connectDB =require('./config/db')
+const authRoutes=require("./routes/authRoutes")
 
 const app=express()
 
@@ -15,12 +16,12 @@ app.use(
     })
 )
 
-app.use(express.json())
+app.use(express.json());
 
 connectDB();
 
+app.use("/api/v1/auth",authRoutes)     //path ek baar dekhna padega
+
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT,()=>{
-    console.log("Server is ruuning on 5000")
-})
+app.listen(PORT,()=>console.log(`Server is ruuning on ${PORT}`))
