5c5a8628030eda4329ba987f78f049f6e0b68173
TechMaestro-k <maheshwarikushagra09@gmail.com>
2025-08-20 22:34:46 +0530

    InfoCard Done

--- frontEnd/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontEnd/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -4,6 +4,11 @@ import { useUserAuth } from '../../hooks/useUserAuth'
 import { useNavigate } from 'react-router-dom'
 import axiosInstance from '../../utils/axiosInstance'
 import { API_PATHS } from '../../utils/apiPaths'
+import InfoCard from '../../components/cards/InfoCard'
+import { LuWalletMinimal,LuHandCoins } from 'react-icons/lu'
+import {IoMdCard} from 'react-icons/io'
+import { addThousandsSeperator } from '../../utils/helper'
+import RecentTransactions from '../../components/Dashboard/RecentTransactions'
 
 const Home = () => {
   useUserAuth()
@@ -12,10 +17,10 @@ const Home = () => {
   const [loading,setLoading]=useState(false)
 
 
-  const fetchDashBoardData=async ()=>{
+  const fetchDashboardData=async ()=>{
     if(loading) return;
 
-    setLoading=true;
+    setLoading(true);
     try{
       const response=await axiosInstance.get(`${API_PATHS.DASHBOARD.GET_DATA}`)
       if(response.data){
@@ -35,7 +40,37 @@ const Home = () => {
 
   return (
     <DashboardLayout activeMenu="Dashboard">
-      <div className='my-5 mx-auto'>Home</div>
+      <div className='my-5 mx-auto'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+          <InfoCard
+            icon={<IoMdCard/>}
+            label="Total Balance"
+            value={addThousandsSeperator(dashboardData?.totalBalance || 0)}
+            color="bg-primary"
+            />
+
+            <InfoCard
+            icon={<LuWalletMinimal/>}
+            label="Total Income"
+            value={addThousandsSeperator(dashboardData?.totalIncome || 0)}
+            color="bg-orange-500"
+            />
+
+            <InfoCard
+            icon={<LuHandCoins/>}
+            label="Total Expense"
+            value={addThousandsSeperator(dashboardData?.totalExpense || 0)}
+            color="bg-red-500"
+            />
+        </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+          <RecentTransactions
+            value={dashboardData?.recentTransactions}
+            onSeeMore={()=>{navigate("/expense")}}
+          />
+        </div>
+      </div>
     </DashboardLayout>
   )
 }
