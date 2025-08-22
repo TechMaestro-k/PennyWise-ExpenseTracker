7ddea988cd32a80026e6d6aee1c145a604959d49
TechMaestro-k <maheshwarikushagra09@gmail.com>
2025-08-22 23:53:09 +0530

    Pie Chart

---
+++ frontEnd/expense-tracker/src/components/Charts/customLegend.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const customLegend = ({payload}) => {
+  return (
+    <div className=''>
+        {payload.map((entry,index)=>{
+            <div className=''>
+                
+            </div>
+        })}
+    </div>
+  )
+}
+
+export default customLegend
